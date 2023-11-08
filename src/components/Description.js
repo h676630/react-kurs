69b88b8a993410d4dbb2c9c2b4baca7ec0061157
Davidobeng32 <Davidobeng32@gmail.com>
2023-11-08 17:08:01 +0100

    endringer :dog:

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,10 +1,17 @@
-import React from "react";
-import { Typography, Box } from "@mui/material";
+import React, {useState} from "react";
+import { Typography, Box, Button, Slider, Stack, Switch, TextField} from "@mui/material";
+import { VolumeDown, VolumeUp } from "@mui/icons-material";
 
 const Description = () => {
   // Teksten nedenfor kan redigeres av kursdeltakerne for å tilpasse beskrivelsen
   const descriptionText =
     "Start dagen din med oversikt og inspirasjon. Her på Daily får du tilgang til dine viktigste oppgaver, dagens vær og en daglig dose med inspirasjon.";
+
+  const [value, setValue] = useState("")
+  console.log(value)
+  
+  const [checked, setChecked] = useState(false);
+  console.log(checked)
 
   return (
     <Box sx={{ my: 4 }}>
@@ -12,7 +19,32 @@ const Description = () => {
         En ny dag, en ny mulighet
       </Typography>
       <Typography variant="body1">{descriptionText}</Typography>
-    </Box>
+
+      <TextField 
+      id="outlined-basic" 
+      label="Navn" 
+      variant="outlined"
+      value={value}
+      onChange={(e) => setValue(e.target.value)} 
+       />
+
+      <Switch 
+      value={checked}
+      onChange={(e)=> setChecked(e.target.checked)}
+       />
+
+     
+      <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+      <VolumeDown />
+      <Slider aria-label="Volume"/>
+      <VolumeUp />
+      </Stack>
+      <Slider disabled defaultValue={30} aria-label="Disabled slider" />
+
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      </Box>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 
 export default function Header() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" color="success">
       <Toolbar>
         <Typography variant="h6" noWrap>
-          Daily
+          Fireworks
         </Typography>
       </Toolbar>
     </AppBar>
